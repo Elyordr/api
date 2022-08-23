33dcbc19946d8ed9879c53e05178fa87b2d1634b
Elyordr <laplapov23@mail.ru>
2022-08-23 16:51:03 +0500

    Buy&Sell upgrade

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -24,7 +24,8 @@ const url = 'https://test.api.hyssa.udevs.io'
 // const mail = generateString(4) + '@mail.ru'
 const static_otp = '667875'
 const env = Cypress.env
-const user_id = '0017Q00000S8T8fQAF'
+const user_id = '0017Q00000W8PYYQA3'
+const bank_acc = "a1P7Q00000058RbUAI"
 
 describe('Hyssa API test', function(){
           // it('Investor CR', function() {
@@ -85,100 +86,101 @@ describe('Hyssa API test', function(){
           //           })
           // })
 
-          it('TopUp&Withdraw', function() {
+          // it('TopUp&Withdraw', function() {
 
-                //Get pockets info
-            cy.request({
-              method: 'GET',
-              url: url + '/pocket/?user_id=' + user_id
-            })
-            .then(function(response) {
-              Cypress.env('cash_pocket_id', response.body.data.pockets[0].name)
-              cy.log(Cypress.env('cash_pocket_id'))
-              // expect(response.body.data.pockets[0].user_id).to.have.property(user_id)
-              Cypress.env('presum', Math.round(response.body.data.pockets[0].balance.balance))
-              cy.log(Cypress.env('presum'))
-            })
+          //       //Get pockets info
+          //   cy.request({
+          //     method: 'GET',
+          //     url: url + '/pocket/?user_id=' + user_id
+          //   })
+          //   .then(function(response) {
+          //     Cypress.env('cash_pocket_id', response.body.data.pockets[0].name)
+          //     cy.log(Cypress.env('cash_pocket_id'))
+          //     // expect(response.body.data.pockets[0].user_id).to.have.property(user_id)
+          //     Cypress.env('presum', Math.round(response.body.data.pockets[0].balance.balance))
+          //     cy.log(Cypress.env('presum'))
+          //   })
 
-                //Get Top up Tax
-                cy.request({
-                  method: 'POST',
-                  url: url + '/pocket/operation/tax',
-                  body: {
-                      "amount": "100",
-                      "operation_name": "Top Up",
-                      "user_id": user_id
-                  }
-                })
-                .then(function(response) {
-                  Cypress.env('t_tax', Math.round(response.body.data.tax_calculated))
-                  cy.log(Cypress.env('t_tax'))
-                })
+          //       //Get Top up Tax
+          //       cy.request({
+          //         method: 'POST',
+          //         url: url + '/pocket/operation/tax',
+          //         body: {
+          //             "amount": "100",
+          //             "operation_name": "Top Up",
+          //             "user_id": user_id
+          //         }
+          //       })
+          //       .then(function(response) {
+          //         Cypress.env('t_tax', Math.round(response.body.data.tax_calculated))
+          //         cy.log(Cypress.env('t_tax'))
+          //       })
 
-                //TopUp      
-            cy.request({
-              method: 'POST',
-              url: url + '/pocket/operation/top_up',
-              body: {
-                "amount": "100",
-                "currency": "USD",
-                "pocket_id": Cypress.env("cash_pocket_id"),
-                "user_id": user_id
-              }
-            })
-            .then(function(response) {
-              expect(response.body).to.have.property('description', "The request has succeeded")
+          //       //TopUp      
+          //   cy.request({
+          //     method: 'POST',
+          //     url: url + '/pocket/operation/top_up',
+          //     body: {
+          //       "amount": "100",
+          //       "bank_account": bank_acc,
+          //       "currency": "USD",
+          //       "pocket_id": Cypress.env("cash_pocket_id"),
+          //       "user_id": user_id
+          //     }
+          //   })
+          //   .then(function(response) {
+          //     expect(response.body).to.have.property('description', "The request has succeeded")
+          //   })
 
-            })
+          //   cy.request({
+          //     method: 'GET',
+          //     url: url + '/pocket/?user_id=' + user_id
+          //   })
+          //   .then(function(response) {
+          //     expect(Math.round(response.body.data.pockets[0].balance.balance)).to.have.equal(Cypress.env('presum') + Cypress.env('t_tax'))
+          //     Cypress.env('presum', Math.round(response.body.data.pockets[0].balance.balance))
+          //     cy.log(Cypress.env('presum'))
+          //   })
 
-            cy.request({
-              method: 'GET',
-              url: url + '/pocket/?user_id=' + user_id
-            })
-            .then(function(response) {
-              expect(Math.round(response.body.data.pockets[0].balance.balance)).to.have.equal(Cypress.env('presum') + Cypress.env('t_tax'))
-              Cypress.env('presum', Math.round(response.body.data.pockets[0].balance.balance))
-              cy.log(Cypress.env('presum'))
-            })
+          //       //Get Withdraw Tax
+          // cy.request({
+          //   method: 'POST',
+          //   url: url + '/pocket/operation/tax',
+          //   body: {
+          //       "amount": "100",
+          //       "operation_name": "Wihdraw",
+          //       "user_id": user_id
+          //   }
+          // })
+          // .then(function(response) {
+          //   Cypress.env('w_tax', Math.round(response.body.data.tax_calculated))
+          //   cy.log(Cypress.env('w_tax'))
+          // })
 
-                //Get Withdraw Tax
-          cy.request({
-            method: 'POST',
-            url: url + '/pocket/operation/tax',
-            body: {
-                "amount": "100",
-                "operation_name": "Wihdraw",
-                "user_id": user_id
-            }
-          })
-          .then(function(response) {
-            Cypress.env('w_tax', Math.round(response.body.data.tax_calculated))
-            cy.log(Cypress.env('w_tax'))
-          })
+          //       //Withdraw
+          //   cy.request({
+          //     method: 'POST',
+          //     url: url + '/pocket/operation/withdraw',
+          //     body: {
+          //       "amount": "100",
+          //       "bank_account": bank_acc,
+          //       "currency": "USD",
+          //       "pocket_id": Cypress.env("cash_pocket_id"),
+          //       "user_id": user_id
+          //     }
+          //   })
+          //   .then(function(response){
+          //     expect(response.status).to.eq(200)
+          //   })
 
-                //Withdraw
-            cy.request({
-              method: 'POST',
-              url: url + '/pocket/operation/withdraw',
-              body: {
-                "amount": "100",
-                "currency": "USD",
-                "pocket_id": Cypress.env("cash_pocket_id"),
-                "user_id": user_id
-              }
-            })
-            .then(function(response){
-              expect(response.status).to.eq(200)
-            })
-
-            cy.request({
-              method: 'GET',  
-              url: url + '/pocket/?user_id=' + user_id
-            })
-            .then(function(response) {
-              expect(Math.round(response.body.data.pockets[0].balance.balance)).to.have.equal(Cypress.env('presum') - Cypress.env('w_tax'))
-            })
-          })
+          //   cy.request({
+          //     method: 'GET',  
+          //     url: url + '/pocket/?user_id=' + user_id
+          //   })
+          //   .then(function(response) {
+          //     expect(Math.round(response.body.data.pockets[0].balance.balance)).to.have.equal(Cypress.env('presum') - Cypress.env('w_tax'))
+          //   })
+          // })
 
           it('Buy&Sell', function() {
                 //Get pockets info
@@ -193,33 +195,15 @@ describe('Hyssa API test', function(){
                 
             var investor_pocket_id = Cypress.env('invest_pocket_id')
 
-            //     //Get stock price
-            // cy.request({
-            //   method: 'GET',
-            //   url: url + '/integration/fix/get-last-quote/ABCL.NASDAQ'
-            // })
-            // .then(function(response) {
-            //   Cypress.env('stock_price', response.body.data.ticks[0].ask[0].value)
-            //   cy.log(Cypress.env('stock_price'))
-            // })
-
-            // var price = Cypress.env('stock_price')
-
-            //     //Get Buy Tax
-            // cy.request({
-            //   method: 'POST',
-            //   url: url + '/pocket/operation/tax',
-            //   body: {
-            //       "amount": price,
-            //       "operation_name": "Buy",
-            //       "symbol_id": "XPEV.NYSE",
-            //       "user_id": user_id
-            //   }
-            // })
-            // .then(function(response) {
-            //   Cypress.env('b_tax', Math.round(response.body.data.tax_calculated))
-            //   cy.log(Cypress.env('b_tax'))
-            // })
+                //Save purchased stocks count
+            cy.request({
+              method: 'GET',
+              url: url + '/investor/purchased-stock/?investor_id=' + user_id
+            })
+            .then(function(response) {
+              Cypress.env('purchased_stocks', response.body.data.stocks[0].count)
+              cy.log(Cypress.env('purchased_stocks'))
+            })
 
                 //Buy
             cy.request({
@@ -253,6 +237,25 @@ describe('Hyssa API test', function(){
                     }
                 }
                 expect(response.body.data.orders[index].id).to.eq(Cypress.env('order_id'))
+                const status = 'response.body.data.orders[index].status'
+                if ((status == ("filled"))) {
+                  cy.request({
+                    method: 'GET',
+                    url: url + '/investor/purchased-stock/?investor_id=' + user_id
+                  })
+                  .then(function(response) {
+                    expect(response.body.data.stocks[0].count).to.have.equal(Cypress.env('purchased_stocks') + 2) ///
+                  })
+                }
+                else if ((status == ("filled"))) {
+                  cy.request({
+                    method: 'GET',
+                    url: url + '/investor/purchased-stock/?investor_id=' + user_id
+                  })
+                  .then(function(response) {
+                    expect(response.body.data.stocks[0].count).to.have.equal(Cypress.env('purchased_stocks') + 2) ///
+                  })
+                }
               })
 
               //Sell
